775d0a6b91543bd3fd0ec0465c63ede181ba2762
Intel <102398177+Intel-Projects@users.noreply.github.com>
2022-05-19 09:55:05 +0100

    Add files via upload

---
+++ lecture.py
@@ -0,0 +1,34 @@
+# Print #
+print("Hello World")
+
+# Variables #
+
+x = 10 # Integer
+y = "Hello World" # String
+z = True # Boolean
+list = ["Hello", "World", "!"] # List
+dict = { 
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+} # Dictionary
+
+# Print Variables #
+print(x)
+print(y)
+print(z)
+print(list)
+print(dict)
+
+# If Statment #
+if x == 10:
+    # This Code Runs IF x = 10!
+    print("X is 10!")
+
+if z is True:
+    # If Z is True
+    print("Z Is True!")
+
+# Input #
+Y = input("Enter < ")#
+print(Y)
